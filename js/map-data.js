e9264157434667bf0e460e8d4bcb85abc55a6c89
thewonderfool <your-email@domain.com>
2026-02-18 20:58:47 -0600

    feat: Integrate Leaflet map library with new level view images and map data.

---
+++ js/map-data.js
@@ -0,0 +1,36 @@
+// Data Points (Calibrated from real map image)
+// Coordinates: [Lat (Y), Lng (X)] where [0,0] is bottom-left
+
+window.tourPoints = [
+    {
+        id: 1,
+        lat: 517.20,
+        lng: 675.00,
+        title: "Park & Wash Facility",
+        // INSTRUCTIONS: To add new photos, upload them to 'images/level_view/'
+        // and add the filename to this list as a new line.
+        // Example: "images/level_view/my-new-photo.jpg",
+        images: [
+            "images/level_view/ChatGPT Image Feb 18, 2026, 08_39_18 PM.png",
+            "images/level_view/IMG_20260218_141405.jpg",
+            "images/level_view/IMG_20260218_141509.jpg",
+            "images/level_view/IMG_20260218_141521.jpg",
+            "images/level_view/IMG_20260218_141528.jpg",
+            "images/level_view/IMG_20260218_141712.jpg",
+            "images/level_view/IMG_20260218_141714.jpg",
+            "images/level_view/IMG_20260218_141718.jpg",
+            "images/level_view/IMG_20260218_141732.jpg",
+            "images/level_view/IMG_20260218_141940.jpg",
+            "images/level_view/IMG_20260218_141951.jpg",
+            "images/level_view/IMG_20260218_141953.jpg",
+            "images/level_view/IMG_20260218_142001.jpg",
+            "images/level_view/IMG_20260218_142003.jpg",
+            "images/level_view/IMG_20260218_142010.jpg",
+            "images/level_view/IMG_20260218_142028.jpg",
+            "images/level_view/IMG_20260218_142036.jpg",
+            "images/level_view/IMG_20260218_142042.jpg",
+            "images/level_view/IMG_20260218_142044.jpg",
+            "images/level_view/IMG_20260218_142048.jpg"
+        ]
+    }
+];
